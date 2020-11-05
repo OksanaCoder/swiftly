76d42e845c5607363b7599baccfbb0afe8f9a554
Oksana <myzove@mail.ru>
2020-11-05 16:31:17 +0200

    adding welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,16 +2,14 @@ import React, { Component } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import back from '../images/back.png'
 import NavBar from './NavBar'
-import Typed from 'typed.js';
+import Typed from 'react-typed';
+import { render } from 'react-dom';
 
 
 
-const Welcome = () => {
-    var options = {
-        strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-        typeSpeed: 40
-      };
-      var typed = new Typed('.element', options);
+class Welcome extends Component  {
+    render() {
+
     return(
         <>
         <div className='welcome'>
@@ -19,10 +17,24 @@ const Welcome = () => {
             <Container>
                 <Row style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingTop: '80px'}}>
                     <Col lg={6} md={6} sm={12}>
-                        <h2 style={{letterSpacing: '3px'}}>{typed}World's 1st E-ID verified</h2>
-                        <h1 style={{color: '#1E1E1E', fontWeight: '800'}}>Live Learning and Automated Recruiting platform</h1>
+                    <h1 style={{fontSize: '60px', fontWeight: '800'}}>Swiftly.
+              <Typed
+                strings={[
+                    'Learn',
+                    'Mentor',
+                    'Hire']}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    loop
+                    style={{color: '#EB3F3F', fontSize: '60px', fontWeight: '600'}}
+                    >
+                </Typed>
+                </h1>
+                        <h2 style={{letterSpacing: '3px', marginTop: '20px'}}>World's 1st E-ID verified</h2>
+                        <h2 style={{ color: '#1E1E1E', margin: '20px 0 20px', fontSize: '23px'}}>Live Learning and Automated Recruiting platform</h2>
                         <h6 style={{color: '#8B8E92', fontSize: '20px', marginTop: '20px', marginBottom: '20px'}}>Post jobs, review applications, schedule interviews
 and hire, all at one place.</h6>
+
                         <Button className='btn-main'>How it works
                         
                         </Button>
@@ -37,6 +49,7 @@ and hire, all at one place.</h6>
         </>
         
     )
+}
 }
 
 export default Welcome
